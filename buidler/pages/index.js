6b7cf0b9f717403a6523ef73720143e8219a994a
balajipachai <balaji.pachai08@gmail.com>
2020-07-12 23:28:19 +0530

    Added client side page navigation

--- buidler/pages/index.js
+++ buidler/pages/index.js
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 export default function Index() {
     return (
         <div>
-            <p>Hello Buidler from Next.js</p>
+            <h1>Copyright Registration</h1>
+            <h2>
+                <Link href="/store">
+                    <a>Store Copyright</a>
+                </Link>
+            </h2>
         </div>
     );
 }
--- buidler/pages/store.js
+++ buidler/pages/store.js
@@ -3,17 +3,24 @@ const style = {
     "margin-top": "20%"
 };
 
+import Link from 'next/link';
+
 export default function Store() {
-    return(
+    return (
         <div id="main" style={style}>
-        <form>
-            <p>Owner Ethereum Public Key</p>
-            <input type="text" id="ownerAddr" name="ownerAddr"/><br></br>
-            <p>Please add timestamp</p>
-            <input type="text" id="timestamp" name="timestamp"/><br></br>
-            <p>Select File to Copyright</p>
-            <input type="file" id="file" name="file"/><br></br>
-        </form>
+            <form>
+                <p>Owner Ethereum Public Key</p>
+                <input type="text" id="ownerAddr" name="ownerAddr" /><br></br>
+                <p>Please add timestamp</p>
+                <input type="text" id="timestamp" name="timestamp" /><br></br>
+                <p>Select File to Copyright</p>
+                <input type="file" id="file" name="file" /><br></br>
+            </form>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
         </div>
     );
 }
